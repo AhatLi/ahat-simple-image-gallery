f3d993d1b5901aeecf475b8c3567b47111ee0c05
ahat <moonks830@gmail.com>
2023-07-20 07:50:22 +0900

    webp 사용할 수 있도록 수정

--- Utils.go
+++ Utils.go
@@ -18,12 +18,13 @@ import (
 	"time"
 
 	"github.com/disintegration/imaging"
+	_ "golang.org/x/image/webp"
 	"gopkg.in/ini.v1"
 )
 
 var fileMap = make(map[string]int)
 
-//FileNameSort : img file sort for name
+// FileNameSort : img file sort for name
 type FileNameSort []os.FileInfo
 
 func (a FileNameSort) Len() int      { return len(a) }
@@ -39,7 +40,7 @@ func (a FileNameSort) Less(i, j int) bool {
 	return a[i].Name() < a[j].Name()
 }
 
-//FileDateSort : img file sort for file last mod date
+// FileDateSort : img file sort for file last mod date
 type FileDateSort []os.FileInfo
 
 func (a FileDateSort) Len() int      { return len(a) }
@@ -56,7 +57,7 @@ func (a FileDateSort) Less(i, j int) bool {
 	return a[i].ModTime().Unix() < a[j].ModTime().Unix()
 }
 
-//FileSizeSort : img file sort for file size
+// FileSizeSort : img file sort for file size
 type FileSizeSort []os.FileInfo
 
 func (a FileSizeSort) Len() int      { return len(a) }
@@ -72,7 +73,7 @@ func (a FileSizeSort) Less(i, j int) bool {
 	return a[i].Size() < a[j].Size()
 }
 
-//이미지를 base64 텍스트로 변환한다.
+// 이미지를 base64 텍스트로 변환한다.
 func imgToBase64(file string) string {
 	f, _ := os.Open(file)
 	reader := bufio.NewReader(f)
@@ -83,13 +84,13 @@ func imgToBase64(file string) string {
 	return "data:image/png;base64," + encoded
 }
 
-//파일 존재여부 확인
+// 파일 존재여부 확인
 func fileExists(filename string) bool {
 	_, err := os.Stat(filename)
 	return !os.IsNotExist(err)
 }
 
-//폴더 구조를 확인하는 함수
+// 폴더 구조를 확인하는 함수
 func getDirPath(path string, dir *[]string) {
 	files, errf := ioutil.ReadDir(path)
 	if errf != nil {
@@ -119,7 +120,7 @@ func preExplorerDirectory(path string) {
 	time.Sleep(time.Millisecond * 10)
 }
 
-//폴더를 탐색하여 이미지가 썸네일이 존재하지 않을 경우 썸네일 파일을 생성한다.
+// 폴더를 탐색하여 이미지가 썸네일이 존재하지 않을 경우 썸네일 파일을 생성한다.
 func explorerDirectory(path string) {
 	os.MkdirAll(thumPath+path, os.ModePerm)
 	files, errf := ioutil.ReadDir(path)
@@ -146,7 +147,7 @@ func explorerDirectory(path string) {
 	time.Sleep(time.Millisecond * 1)
 }
 
-//썸네일을 생성한다. 현재 이미지의 특정색이 완전히 검은색인 경우 썸네일에서 흰색으로 표시되는 문제점이 있는것으로 보임.
+// 썸네일을 생성한다. 현재 이미지의 특정색이 완전히 검은색인 경우 썸네일에서 흰색으로 표시되는 문제점이 있는것으로 보임.
 func makeThumbnail(filename string, filecheck bool) {
 	thumbname := thumPath + filename
 
@@ -181,7 +182,7 @@ func makeThumbnail(filename string, filecheck bool) {
 	}
 }
 
-//유저 로그인을 위한 설정을 받아온다.
+// 유저 로그인을 위한 설정을 받아온다.
 func getUserData() (string, string) {
 	cfg, err := ini.Load("ImageCloud.conf")
 	if err != nil {
@@ -193,7 +194,7 @@ func getUserData() (string, string) {
 	return confUsername, confPasswd
 }
 
-//환경 관련 설정을 받아온다.
+// 환경 관련 설정을 받아온다.
 func getEnvData() string {
 	cfg, err := ini.Load("ImageCloud.conf")
 	if err != nil {
@@ -208,7 +209,7 @@ func getEnvData() string {
 	return html
 }
 
-//이미지 표시를 위한 설저응ㄹ 받아온다.
+// 이미지 표시를 위한 설저응ㄹ 받아온다.
 func getContentData() (int, string) {
 	cfg, err := ini.Load("ImageCloud.conf")
 	if err != nil {
@@ -228,7 +229,7 @@ func getContentData() (int, string) {
 	return count, sort
 }
 
-//클라이언트가 설정을 변경하였을 경우 해당 설정을 파일에 반영한다.
+// 클라이언트가 설정을 변경하였을 경우 해당 설정을 파일에 반영한다.
 func setContentData(count string, sort string) {
 	cfg, err := ini.Load("ImageCloud.conf")
 	if err != nil {
@@ -240,7 +241,7 @@ func setContentData(count string, sort string) {
 	cfg.SaveTo("ImageCloud.conf")
 }
 
-//설정파일에서 포트번호를 받아온다. 기본 포트주소는 9090
+// 설정파일에서 포트번호를 받아온다. 기본 포트주소는 9090
 func getPort() string {
 	cfg, err := ini.Load("ImageCloud.conf")
 	if err != nil {
@@ -254,7 +255,7 @@ func getPort() string {
 	return ":" + cfg.Section("network").Key("port").String()
 }
 
-//파일을 검색했을 경우 해당 텍스트로 파일을 필터링한다. 추후 와일드카드 등 추가 예정
+// 파일을 검색했을 경우 해당 텍스트로 파일을 필터링한다. 추후 와일드카드 등 추가 예정
 func fileSearch(files []os.FileInfo, text string) []os.FileInfo {
 	result := make([]os.FileInfo, 0)
 
@@ -267,7 +268,7 @@ func fileSearch(files []os.FileInfo, text string) []os.FileInfo {
 	return result
 }
 
-//이미지 파일만을 표시하기 위해서 필터링을 하는 동작
+// 이미지 파일만을 표시하기 위해서 필터링을 하는 동작
 func imgFilter(files []os.FileInfo, text string) []os.FileInfo {
 	result := make([]os.FileInfo, 0)
 
@@ -288,17 +289,17 @@ func imgFilter(files []os.FileInfo, text string) []os.FileInfo {
 	return result
 }
 
-//현재는 네가지 파일 확장자만을 지원한다.
+// 현재는 다섯가지 파일 확장자만을 지원한다.
 func isImage(filename string) bool {
-	return strings.HasSuffix(filename, ".jpg") || strings.HasSuffix(filename, ".png") || strings.HasSuffix(filename, ".gif") || strings.HasSuffix(filename, ".jpeg")
+	return strings.HasSuffix(filename, ".jpg") || strings.HasSuffix(filename, ".png") || strings.HasSuffix(filename, ".gif") || strings.HasSuffix(filename, ".jpeg") || strings.HasSuffix(filename, ".webp")
 }
 
-//엑세스 로그를 출력하는 함수
+// 엑세스 로그를 출력하는 함수
 func printLog(r *http.Request) {
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05") + "," + r.RemoteAddr + "," + r.URL.Path)
 }
 
-//파일 삭제 동작을 하는 함수
+// 파일 삭제 동작을 하는 함수
 func fileRemove(files string, path string) {
 	filesSplit := strings.Split(files, ",")
 	for _, file := range filesSplit {
@@ -317,7 +318,7 @@ func fileRemove(files string, path string) {
 	}
 }
 
-//파일 이동 동작을 하는 함수
+// 파일 이동 동작을 하는 함수
 func fileMove(files string, source string, dest string) {
 	filesSplit := strings.Split(files, ",")
 	for _, file := range filesSplit {
